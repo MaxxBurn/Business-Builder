b2238db7f4c63fc79c4e74f9c938eaabafae4418
tarka <tarkaklavio@gmail.com>
2020-06-21 18:40:52 +0200

    Added the 4th menu of 2nd section

--- app/src/main/java/com/example/businessbuilder/PlanningAction4.kt
+++ app/src/main/java/com/example/businessbuilder/PlanningAction4.kt
@@ -2,8 +2,11 @@ package com.example.businessbuilder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CheckBox
 
 class PlanningAction4 : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_planning_action4)
