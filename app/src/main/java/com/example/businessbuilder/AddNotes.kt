a148c48d59caadf929b01a0f5117b37a0059be61
tarka <tarkaklavio@gmail.com>
2020-06-30 12:54:31 +0200

    end of 06/30/2020

--- app/src/main/java/com/example/businessbuilder/AddNotes.kt
+++ app/src/main/java/com/example/businessbuilder/AddNotes.kt
@@ -1,11 +1,29 @@
 package com.example.businessbuilder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class AddNotes : AppCompatActivity() {
+
+    lateinit var button1: FloatingActionButton
+    lateinit var button2: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_notes)
+
+        button1 = findViewById(R.id.addNotesButton)
+        button2 = findViewById(R.id.searchNotesButton)
+
+        button1.setOnClickListener {
+            val intent = Intent(this, NoteMenu::class.java)
+            startActivity(intent)
+        }
+        button2.setOnClickListener {
+
+        }
+
     }
 }
